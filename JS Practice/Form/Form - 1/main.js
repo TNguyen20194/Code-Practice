149ffa0737a6959cbae5126ae65e2a046a781dde
Truong Nguyen <truong.nguyen201@gmail.com>
2026-01-29 15:39:52 -0500

    Update file

---
+++ JS Practice/Form/Form - 1/main.js
@@ -0,0 +1,18 @@
+const generateUniqueString = (length) => {
+    return Math.random()
+    .toString(36)
+    .substring(2, 2 + length)
+}
+
+const fruits = ["Apple", "Banada", "Orange"];
+const ul = document.querySelector("ul");
+
+fruits.forEach((fruit, index) => {
+    const li = document.createElement("li");
+    li.textContent = fruit;
+    li.id = generateUniqueString(5)
+
+    ul.appendChild(li);
+})
+
+console.log(ul)
