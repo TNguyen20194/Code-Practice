a980aea7cb2c2912c0877c63e8662462564510a4
Truong Nguyen <truong.nguyen201@gmail.com>
2026-01-30 14:49:36 -0500

    Todo list

---
+++ JS Practice/Form/To-do-list/js/main.js
@@ -0,0 +1,43 @@
+const todoList = document.querySelector(".todolist");
+const taskList = todoList.querySelector(".todolist__tasks")
+
+
+function generateUniqueString(length) {
+  return Math.random()
+    .toString(36)
+    .substring(2, 2 + length)
+}
+
+todoList.addEventListener("submit", e => {
+    e.preventDefault();
+
+    // Get value of task
+    const newTaskField = todoList.querySelector("input");
+    const inputValue = newTaskField.value.trim();
+
+    // Create task
+    const uniqueID = generateUniqueString(10);
+    const taskElement = document.createElement('li');
+    taskElement.classList.add("task");
+    taskElement.innerHTML = DOMPurify.sanitize(`
+        <input type="checkbox" id="${uniqueID}">
+          <label for="${uniqueID}">
+            <svg viewBox="0 0 20 15">
+              <path d="M0 8l2-2 5 5L18 0l2 2L7 15z" fill-rule="nonzero" />
+            </svg>
+          </label>
+          <span class="task__name">${inputValue}</span>
+          <button type="button" class="task__delete-button">
+            <svg viewBox="0 0 20 20">
+              <path
+                d="M10 8.586L2.929 1.515 1.515 2.929 8.586 10l-7.071 7.071 1.414 1.414L10 11.414l7.071 7.071 1.414-1.414L11.414 10l7.071-7.071-1.414-1.414L10 8.586z"
+              />
+            </svg>
+          </button>
+    `);
+
+    
+
+})
+
+console.log(taskList)
