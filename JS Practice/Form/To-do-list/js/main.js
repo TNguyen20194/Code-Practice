e5ccd9a45933481c7a5ca7fe8dad732d33e6bc95
Truong Nguyen <truong.nguyen201@gmail.com>
2026-01-30 15:14:07 -0500

    Refactor todoList task creation

--- JS Practice/Form/To-do-list/js/main.js
+++ JS Practice/Form/To-do-list/js/main.js
@@ -6,6 +6,30 @@ function generateUniqueString(length) {
   return Math.random()
     .toString(36)
     .substring(2, 2 + length)
+};
+
+function makeTaskElement(taskname) {
+    const uniqueID = generateUniqueString(10);
+    const taskElement = document.createElement('li');
+    taskElement.classList.add("task");
+    taskElement.innerHTML = DOMPurify.sanitize(`
+        <input type="checkbox" id="${uniqueID}">
+            <label for="${uniqueID}">
+            <svg viewBox="0 0 20 15">
+                <path d="M0 8l2-2 5 5L18 0l2 2L7 15z" fill-rule="nonzero" />
+            </svg>
+            </label>
+            <span class="task__name">${taskname}</span>
+            <button type="button" class="task__delete-button">
+            <svg viewBox="0 0 20 20">
+                <path
+                d="M10 8.586L2.929 1.515 1.515 2.929 8.586 10l-7.071 7.071 1.414 1.414L10 11.414l7.071 7.071 1.414-1.414L11.414 10l7.071-7.071-1.414-1.414L10 8.586z"
+                />
+            </svg>
+            </button>
+    `);
+
+    return taskElement;
 }
 
 todoList.addEventListener("submit", e => {
@@ -15,29 +39,17 @@ todoList.addEventListener("submit", e => {
     const newTaskField = todoList.querySelector("input");
     const inputValue = newTaskField.value.trim();
 
+    // Clear the new task field
+    newTaskField.value = "";
+
+    // Bring focus back to the input field
+    newTaskField.focus();
+
+    // Prevent adding of empty task
+    if(!inputValue) return;
+
     // Create task
-    const uniqueID = generateUniqueString(10);
-    const taskElement = document.createElement('li');
-    taskElement.classList.add("task");
-    taskElement.innerHTML = DOMPurify.sanitize(`
-        <input type="checkbox" id="${uniqueID}">
-          <label for="${uniqueID}">
-            <svg viewBox="0 0 20 15">
-              <path d="M0 8l2-2 5 5L18 0l2 2L7 15z" fill-rule="nonzero" />
-            </svg>
-          </label>
-          <span class="task__name">${inputValue}</span>
-          <button type="button" class="task__delete-button">
-            <svg viewBox="0 0 20 20">
-              <path
-                d="M10 8.586L2.929 1.515 1.515 2.929 8.586 10l-7.071 7.071 1.414 1.414L10 11.414l7.071 7.071 1.414-1.414L11.414 10l7.071-7.071-1.414-1.414L10 8.586z"
-              />
-            </svg>
-          </button>
-    `);
+    const taskElement = makeTaskElement(inputValue);
 
-    
-
-})
-
-console.log(taskList)
+    taskList.appendChild(taskElement);
+});
